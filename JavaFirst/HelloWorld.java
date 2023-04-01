fdfc04675065c862085b8a16df76790f954978d8
dan <dancity7@gmail.com>
2023-03-31 21:39:34 -0500

    first code

---
+++ JavaFirst/HelloWorld.java
@@ -0,0 +1,43 @@
+package JavaFirst;
+
+public class HelloWorld {
+
+
+    public static void main (String [] args){
+
+
+        System.out.println("Hello World");
+        System.out.println("We did it");
+        System.out.println("My name is Dan Draghici");
+        System.out.println("*****Hi*****");
+        System.out.println (5+3);
+        System.out.println (5+3+" is 8");
+        System.out.println("5*3="+5 * 3);
+        System.out.println("5 + 3 = " +5 +8);
+
+
+        System.out.println(2 + 3 );
+        System.out.println("2 + 3");
+        System.out.println("Hello" + 9);
+        System.out.println("Hello" + "Jake");
+        System.out.println("5 + 3 =" +  5  + "-----" +8);
+        System.out.println("2 times 4 is " + 2 * 4);
+        System.out.println("now example for subtraction "+ (6-3));
+        System.out.println("result is --->"+ 234454 * 35336566);
+        //taking notes in intelliJ without effect on the code using double /
+        /*
+        thisgit
+        is
+        for
+        multiple lines of comment
+         */
+
+
+    }
+
+
+
+    }
+
+
+
